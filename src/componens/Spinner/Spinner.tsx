2f21528aad2ded0868c954c1795049a28bcdc274
Sheverda <nsewerda04@gmail.com>
2023-01-21 12:18:06 +0200

    resresh

--- src/componens/Spinner/Spinner.tsx
+++ src/componens/Spinner/Spinner.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import "./spinner-loading.css"
 const Spinner = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
     return (
         <div className={"spinner-loading"} >
             <div className="spinner-loading-content">
