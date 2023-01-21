3fbf2184dc9c1ed380f7fe805a35c524d691342f
Sheverda <nsewerda04@gmail.com>
2023-01-21 23:34:12 +0200

    localhost

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {AuthResponse} from "../models/AuthResponse";
 
-const API_URL = "https://server-monographs.vercel.app/api"
+const API_URL = "http://localhost:5000/api"
 
 const $api = axios.create({
     withCredentials: true,
@@ -13,7 +13,6 @@ $api.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${localStorage.getItem("accessToken")}`
     return config
 })
-
 
 $api.interceptors.response.use((config) => {
     return config;
